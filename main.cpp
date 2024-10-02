073ac22779f152926e9fe87327040cb4f1884d06
stickv <1602427689@qq.com>
2024-10-02 22:06:01 +0800

    update read me

--- main.cpp
+++ main.cpp
@@ -5,6 +5,5 @@
 int main()
 {
     http::httprecv answer_("127.0.0.1",8082);
-    answer_.Reverse_proxy("127.0.0.1",7000)
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 开始学习github了
 
-10.2 学习http报文，cmake 构建 demo 
+10.2 学习http报文，cmake 构建 demo （好多bug）
